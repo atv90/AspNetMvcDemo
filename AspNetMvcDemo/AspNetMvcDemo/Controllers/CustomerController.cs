e76975d19f79bb6aa564a9475d09703d70ba803c
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2017-12-22 15:47:51 +0200

    Luotiin Customer-kontrolleri, kontrollerin kautta index-näkymä, tuotiin näkymään kontrollerin tietoja ViewBag-metodilla ja malliolion kautta.

---
+++ AspNetMvcDemo/AspNetMvcDemo/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using AspNetMvcDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetMvcDemo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            ViewBag.OmaTieto = "ABC123";
+
+            NorthwindEntities entities = new NorthwindEntities();
+            //muodostetaan asiakastaulun tiedoista model-niminen lista ja pyydetään entities-oliosta Customers-entities-kokoelmasta
+            //kokonaisuutena listaksi
+            List<Customers> model = entities.Customers.ToList();
+            entities.Dispose();
+
+            //palautetaan näkymälle model-olio
+            return View(model);
+        }
+    }
+}
